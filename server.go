1beeb81d26ed4d4194ca525d0cda36c55e8b217b
barry.paneer <barry.paneer@qq.com>
2018-04-11 14:22:38 +0800

    updates

--- server.go
+++ server.go
@@ -1,40 +1,34 @@
 package main
 
-
 import (
-	"os"
-	"log"
-	"fmt"
 	"flag"
+	"log"
+	"os"
 )
 
+var (
+	sIP   string
+	nPort int
+)
 
 func init() {
-
+	// parse arguments from command line
+	flag.IntVar(&nPort, "port", 31256, "listen port")
+	flag.StringVar(&sIP, "ip", "127.0.0.1", "server ip address")
+	flag.Parse()
 }
-
 
 func main() {
 	// set log file
-	oLogFile, oLogErr := os.OpenFile( "./Server.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666 )
+	oLogFile, oLogErr := os.OpenFile("./Server.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 	if oLogErr != nil {
-		log.Fatal( "main() : an error occur while creating log file !" )
-		os.Exit( 1 )
+		log.Fatal("main() : an error occur while creating log file !")
+		os.Exit(1)
 	}
 
-	flag.Parse()
-	log.SetOutput( oLogFile )
-	log.Println( "[Begin] ##################################" )
+	log.SetOutput(oLogFile)
+	log.Println("[Begin] ##################################")
+	log.Println("[INF] Server IP:port -->", sIP, nPort)
 
-	// parse arguments
-	pszIP := flag.String( "ip", "127.0.0.1", "ip address" )
-	pnPort := flag.Int( "port", 31256, "port" )
-
-	fmt.Println( *pszIP, *pnPort )
-
-	log.Println( "[ End ] ##################################" )
+	log.Println("[ End ] ##################################")
 }
-
-
-
-
