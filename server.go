ceb1cf0d0d0f41cb429c5820838ab4ffa42c62d6
barry.paneer <barry.paneer@qq.com>
2018-04-11 18:05:13 +0800

    updates

--- server.go
+++ server.go
@@ -1,4 +1,9 @@
-// program entry ...............
+/**
+ * @brief		entry file of program
+ * @detail		files sync server
+ * @author		barry
+ * @date		2018/4/10
+ */
 package main
 
 import (
@@ -9,11 +14,11 @@ import (
 )
 
 var (
-	sIP      string // Server IP
-	nPort    int    // Server Port
-	bDumpLog bool   // Switch 4 Log Dump
-	sLogFile string // Log File Path
-	sXmlCfg  string // Configuration File (.xml)
+	sIP         string // Server IP
+	nPort       int    // Server Port
+	bDumpLog    bool   // Switch 4 Log Dump
+	sLogFile    string // Log File Path
+	sSyncFolder string // Sync File Folder
 )
 
 // package imitialization
@@ -23,7 +28,7 @@ func init() {
 	flag.StringVar(&sIP, "ip", "127.0.0.1", "file sync server's ip address (default:127.0.0.1)")
 	flag.BoolVar(&bDumpLog, "dumplog", false, "a switch 4 log dump (default:false)")
 	flag.StringVar(&sLogFile, "logpath", "./server.log", "log file's path (default:./Server.log)")
-	flag.StringVar(&sXmlCfg, "cfg", "./server.xml", "configuration of server (.xml)")
+	flag.StringVar(&sSyncFolder, "cfg", "./SyncFolder/", "data folder 4 sync")
 	flag.Parse()
 }
 
@@ -34,7 +39,7 @@ func main() {
 		oLogFile, oLogErr := os.OpenFile(sLogFile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 		if oLogErr != nil {
 			log.Fatal("[ERR] main() : an error occur while creating log file ! ", sLogFile)
-			os.Exit(1)
+			os.Exit(1) // abort
 		}
 
 		log.SetOutput(oLogFile)
@@ -43,7 +48,7 @@ func main() {
 	//////////////// Active File Sync Server
 	log.Println("[INF] [Begin] ##################################")
 	log.Println("[INF] Server IP:Port -->", sIP, nPort)
-	log.Println("[INF] Server Configuration Path -->", sXmlCfg)
+	log.Println("[INF] Sync Folder -->", sSyncFolder)
 
 	fserver.RunServer()
 
