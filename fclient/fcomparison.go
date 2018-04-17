ab61ae51487a940d29ee5727f6062b75e40581ed
barry.paneer <barry.paneer@qq.com>
2018-04-17 14:47:46 +0800

    updates

--- fclient/fcomparison.go
+++ fclient/fcomparison.go
@@ -6,11 +6,7 @@
 package fclient
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
-	"strings"
-	"time"
 )
 
 var ()
@@ -21,24 +17,19 @@ func init() {
 
 ///////////////////////////////////// HTTP Client Engine Stucture/Class
 type FComparison struct {
-	ServerHost string // Server IP + Port
-	Account    string // Server Login Username
-	Password   string // Server Login Password
-	TTL        int    // Time To Live
 }
 
 ///////////////////////////////////// [OutterMethod]
 //  Active HTTP Client
-func (pSelf *FileSyncClient) DoTasks() {
-	log.Println("[INF] FileSyncClient.DoTasks() : Executing Tasks ...... ")
+func (pSelf *FComparison) DoTasks() {
+	log.Println("[INF] FComparison.DoTasks() : Executing Tasks ...... ")
 
 }
 
 ///////////////////////////////////// [InnerMethod]
 // [method] download resource
 func (pSelf *FComparison) fetchResource(sUri, sMD5, sDateTime string) {
-	log.Println("[INF] FileSyncClient.fetchResource() : [Downloading] -->", sUri, sMD5, sDateTime)
+	log.Println("[INF] FComparison.fetchResource() : [Downloading] -->", sUri, sMD5, sDateTime)
 
-	log.Println("[INF] FileSyncClient.fetchResource() : [Complete]")
-	pSelf.objChannel <- DownloadStatus{URI: sUri, Status: ST_Completed} // Mission Complete!
+	log.Println("[INF] FComparison.fetchResource() : [Complete]")
 }
