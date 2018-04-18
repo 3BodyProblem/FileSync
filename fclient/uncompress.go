63d7ab7da7e4f23a3fe6aab45bbad637c2241400
barry.paneer <barry.paneer@qq.com>
2018-04-18 16:54:05 +0800

    updates

--- fclient/uncompress.go
+++ fclient/uncompress.go
@@ -6,17 +6,10 @@
 package fclient
 
 import (
-	"crypto/md5"
-	"fmt"
-	"io"
+/*	"fmt"
 	"log"
-	"os"
 	"path/filepath"
-	"strings"
-)
-
-var (
-	CacheFolder string = "./FileCache" // cache folder of the program
+	"strings"*/
 )
 
 // Package Initialization
@@ -24,57 +17,13 @@ func init() {
 }
 
 ///////////////////////////////////// HTTP Client Engine Stucture/Class
-type FComparison struct {
-	URI      string // Resource URI in server
-	MD5      string // MD5 of Res
-	DateTime string // UpdateTime Of Res
+type Uncompress struct {
+	TargetFolder string // Root Folder
 }
 
 ///////////////////////////////////// [OutterMethod]
-// [method] Compare Resource Files Between Server & Client
-func (pSelf *FComparison) Compare() bool {
-	// get absolute path of current working folder && build it
-	sLocalFolder, err := filepath.Abs((filepath.Dir("./")))
-	if err != nil {
-		log.Println("[WARN] FComparison.Compare() : failed 2 fetch absolute path of program")
-		return false
-	}
-
-	sLocalFolder = filepath.Join(sLocalFolder, CacheFolder)
-	log.Printf("[INF] FComparison.Compare() : [Comparing]    (%s)   VS   (%s) ", pSelf.URI, sLocalFolder)
-
-	err = os.MkdirAll(sLocalFolder, 0777)
-	if err != nil {
-		log.Printf("[WARN] FComparison.Compare() : failed 2 create folder : %s : %s", sLocalFolder, err.Error())
-		return false
-	}
-
-	// get absolute path of URI in local machine
-	sLocalFile := filepath.Join(sLocalFolder, pSelf.URI)
-	objFile, err := os.Open(sLocalFile)
-	if err != nil {
-		log.Println("[INF] FComparison.Compare() : local file is not exist :", sLocalFile)
-		return false
-	}
-
-	// parepare 2 generate md5
-	defer objFile.Close()
-	objMD5Hash := md5.New()
-	if _, err := io.Copy(objMD5Hash, objFile); err != nil {
-		log.Printf("[WARN] FComparison.Compare() : failed 2 generate MD5 : %s : %s", sLocalFolder, err.Error())
-		return false
-	}
-
-	// generate MD5 string
-	var byteMD5 []byte
-	var sMD5Str string = fmt.Sprintf("%x", objMD5Hash.Sum(byteMD5))
-
-	// result
-	if strings.ToLower(pSelf.MD5) != strings.ToLower(sMD5Str) {
-		log.Printf("[INF] FComparison.Compare() : found a discrepancy between md5 of server(md5:%s) && client(md5:%s)", strings.ToLower(pSelf.MD5), strings.ToLower(sMD5Str))
-		return false
-	}
-
+// [method] Unzip
+func (pSelf *Uncompress) Unzip() bool {
 	return true
 }
 
