7d91a0ebfb95b8ca0cfaf4867eb58bb89a24c444
barry.paneer <barry.paneer@qq.com>
2018-04-21 15:24:12 +0800

    updates

--- fserver/compress.go
+++ fserver/compress.go
@@ -40,53 +40,7 @@ func (pSelf *Compress) Zip(sResName string, objDataSrc *DataSourceConfig) bool {
 		log.Printf("[ERR] Compress.Zip() : [Compressing] invalid exchange code(%s) or data type(%s)", objDataSrc.MkID, sDataType)
 		return false
 	}
-	/*
-		sZipSrcPath := ""
-		// open zip file
-		sLocalFolder := path.Dir(filepath.Join(pSelf.TargetFolder, objDataSrc.Folder))
-		objZipReader, err := zip.OpenReader(sZipSrcPath)
-		if err != nil {
-			log.Println("[ERR] Compress.Zip() : [Compressing] cannot open zip file :", sZipSrcPath, err.Error())
-			return false
-		}
 
-		defer objZipReader.Close()
-		for _, objFile := range objZipReader.File {
-			objReadCloser, err := objFile.Open()
-			if err != nil {
-				log.Println("[ERR] Compress.Zip() : [Compressing] cannot open file in zip package, file name =", objFile.Name)
-				return false
-			}
-
-			defer objReadCloser.Close()
-			sTargetFile := filepath.Join(sLocalFolder, objFile.Name)
-			_, sSplitFileName := path.Split(sTargetFile)
-			if strings.Contains(sSplitFileName, ".") == false {
-				continue
-			}
-			sTargetFolder := path.Dir(sTargetFile)
-			err = os.MkdirAll(sTargetFolder, 0755)
-			if err != nil {
-				log.Println("[ERR] Compress.Zip() : [Compressing] cannot build target folder 4 zip file, file name =", sTargetFile)
-				return false
-			}
-
-			objTargetFile, err := os.Create(sTargetFile)
-			if err != nil {
-				log.Println("[ERR] Compress.Zip() : [Compressing] cannot create zip file in target folder, file name =", sTargetFile)
-				return false
-			}
-			defer objTargetFile.Close()
-			_, err = io.Copy(objTargetFile, objReadCloser)
-			if err != nil {
-				log.Println("[ERR] v.Zip() : [Compressing] cannot write date 2 zip file in target folder, file name =", sTargetFile)
-				return false
-			}
-
-			objTargetFile.Close()
-			objReadCloser.Close()
-		}
-	*/
 	return true
 }
 
