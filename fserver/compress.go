855cadb5a5413ddf0a6e045f9f2ef5110111abee
barry.paneer <barry.paneer@qq.com>
2018-04-23 19:02:55 +0800

    updates

--- fserver/compress.go
+++ fserver/compress.go
@@ -39,7 +39,9 @@ func (pSelf *Compress) Zip(sResName string, objDataSrc *DataSourceConfig) bool {
 	case (objDataSrc.MkID == "sse" && sDataType == ".d1") || (objDataSrc.MkID == "szse" && sDataType == ".d1"):
 		pSelf.zipFolder(filepath.Join(sDestFolder, "DAY.zip"), objDataSrc.Folder)
 	case (objDataSrc.MkID == "sse" && sDataType == ".m1") || (objDataSrc.MkID == "szse" && sDataType == ".m1"):
-		pSelf.zipK1Folder(filepath.Join(sDestFolder, "MIN.zip"), objDataSrc.Folder)
+		pSelf.zipM1Folder(filepath.Join(sDestFolder, "MIN.zip"), objDataSrc.Folder)
+	case (objDataSrc.MkID == "sse" && sDataType == ".m5") || (objDataSrc.MkID == "szse" && sDataType == ".m5"):
+		pSelf.zipM5Folder(filepath.Join(sDestFolder, "MIN5.zip"), objDataSrc.Folder)
 	default:
 		log.Printf("[ERR] Compress.Zip() : [Compressing] invalid exchange code(%s) or data type(%s)", objDataSrc.MkID, sDataType)
 		return false
@@ -49,11 +51,103 @@ func (pSelf *Compress) Zip(sResName string, objDataSrc *DataSourceConfig) bool {
 }
 
 ///////////////////////////////////// [InnerMethod]
-// [method] Zip K1
-func (pSelf *Compress) zipK1Folder(sDestFile, sSrcFolder string) bool {
+// [method] Zip M5
+func (pSelf *Compress) zipM5Folder(sDestFile, sSrcFolder string) bool {
 	err := os.MkdirAll(path.Dir(sDestFile), 0755)
 	if err != nil {
-		log.Println("[ERR] Compress.zipK1Folder() : cannot build target folder 4 zip file :", path.Dir(sDestFile))
+		log.Println("[ERR] Compress.zipM5Folder() : cannot build target folder 4 zip file :", path.Dir(sDestFile))
+		return false
+	}
+
+	sSubFolder := "MIN5"
+	objZipFile, err := os.Create(sDestFile)
+	objZipWriter := zip.NewWriter(objZipFile)
+	defer objZipFile.Close()
+	defer objZipWriter.Close()
+
+	log.Printf("[INF] Compress.zipM5Folder() : zipping (%s) --> (%s)", sSrcFolder, sDestFile)
+	if err != nil {
+		log.Println("[ERR] Compress.zipM5Folder() : failed 2 create zip file :", sDestFile, err.Error())
+		return false
+	}
+
+	nToday := time.Now().Year()
+	err = filepath.Walk(sSrcFolder, func(sPath string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+
+		if f.IsDir() {
+			_, sSF := path.Split(sPath)
+			sSubFolder = filepath.Join("MIN5", sSF)
+			return nil
+		}
+
+		// get absolute path of URI in local machine
+		objFile, err := os.Open(sPath)
+		if err != nil {
+			log.Println("[WARN] Compress.zipM5Folder() : local file is not exist :", sPath)
+			return nil
+		}
+
+		defer objFile.Close()
+		info, err := objFile.Stat()
+		objHInfo, err := zip.FileInfoHeader(info)
+		if err != nil {
+			log.Println("[WARN] Compress.zipM5Folder() : failed 2 create file info head :", err.Error())
+			return nil
+		}
+
+		_, sFileName := path.Split(sPath)
+		objHInfo.Name = filepath.Join(sSubFolder, sFileName)
+		objHeader, err := objZipWriter.CreateHeader(objHInfo)
+		if err != nil {
+			log.Println("[WARN] Compress.zipM5Folder() : failed 2 create filehead :", err.Error())
+			return nil
+		}
+
+		bytesData, err := ioutil.ReadAll(objFile)
+		if err != nil {
+			log.Println("[WARN] Compress.zipM5Folder() : failed 2 read file=", sPath)
+			return nil
+		}
+		for _, bLine := range bytes.Split(bytesData, []byte("\n")) {
+			sFirstFields := strings.Split(string(bLine), ",")[0]
+			if len(sFirstFields) <= 0 {
+				continue
+			}
+			nDate, err := strconv.Atoi(sFirstFields)
+			if err != nil {
+				continue
+			}
+			nDate = nDate / 10000
+			if (nToday - nDate) > 1 {
+				continue
+			}
+
+			_, err = objHeader.Write(bLine)
+			if err != nil {
+				log.Println("[WARN] Compress.zipM5Folder() : failed 2 write zip file=", sPath)
+				return nil
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Println("[ERR] Compress.zipM5Folder() : failed 2 walk src folder :", sSrcFolder)
+		return false
+	}
+
+	return true
+}
+
+// [method] Zip M1
+func (pSelf *Compress) zipM1Folder(sDestFile, sSrcFolder string) bool {
+	err := os.MkdirAll(path.Dir(sDestFile), 0755)
+	if err != nil {
+		log.Println("[ERR] Compress.zipM1Folder() : cannot build target folder 4 zip file :", path.Dir(sDestFile))
 		return false
 	}
 
@@ -63,12 +157,13 @@ func (pSelf *Compress) zipK1Folder(sDestFile, sSrcFolder string) bool {
 	defer objZipFile.Close()
 	defer objZipWriter.Close()
 
-	log.Printf("[INF] Compress.zipK1Folder() : zipping (%s) --> (%s)", sSrcFolder, sDestFile)
+	log.Printf("[INF] Compress.zipM1Folder() : zipping (%s) --> (%s)", sSrcFolder, sDestFile)
 	if err != nil {
-		log.Println("[ERR] Compress.zipK1Folder() : failed 2 create zip file :", sDestFile, err.Error())
+		log.Println("[ERR] Compress.zipM1Folder() : failed 2 create zip file :", sDestFile, err.Error())
 		return false
 	}
 
+	nToday := time.Now().Year()*100 + int(time.Now().Month())
 	err = filepath.Walk(sSrcFolder, func(sPath string, f os.FileInfo, err error) error {
 		if f == nil {
 			return err
@@ -83,7 +178,7 @@ func (pSelf *Compress) zipK1Folder(sDestFile, sSrcFolder string) bool {
 		// get absolute path of URI in local machine
 		objFile, err := os.Open(sPath)
 		if err != nil {
-			log.Println("[WARN] Compress.zipK1Folder() : local file is not exist :", sPath)
+			log.Println("[WARN] Compress.zipM1Folder() : local file is not exist :", sPath)
 			return nil
 		}
 
@@ -91,7 +186,7 @@ func (pSelf *Compress) zipK1Folder(sDestFile, sSrcFolder string) bool {
 		info, err := objFile.Stat()
 		objHInfo, err := zip.FileInfoHeader(info)
 		if err != nil {
-			log.Println("[WARN] Compress.zipK1Folder() : failed 2 create file info head :", err.Error())
+			log.Println("[WARN] Compress.zipM1Folder() : failed 2 create file info head :", err.Error())
 			return nil
 		}
 
@@ -99,21 +194,41 @@ func (pSelf *Compress) zipK1Folder(sDestFile, sSrcFolder string) bool {
 		objHInfo.Name = filepath.Join(sSubFolder, sFileName)
 		objHeader, err := objZipWriter.CreateHeader(objHInfo)
 		if err != nil {
-			log.Println("[WARN] Compress.zipK1Folder() : failed 2 create filehead :", err.Error())
+			log.Println("[WARN] Compress.zipM1Folder() : failed 2 create filehead :", err.Error())
 			return nil
 		}
 
-		_, err = io.Copy(objHeader, objFile)
+		bytesData, err := ioutil.ReadAll(objFile)
 		if err != nil {
-			log.Println("[WARN] Compress.zipK1Folder() : failed 2 read file=", sPath)
+			log.Println("[WARN] Compress.zipM1Folder() : failed 2 read file=", sPath)
 			return nil
+		}
+		for _, bLine := range bytes.Split(bytesData, []byte("\n")) {
+			sFirstFields := strings.Split(string(bLine), ",")[0]
+			if len(sFirstFields) <= 0 {
+				continue
+			}
+			nDate, err := strconv.Atoi(sFirstFields)
+			if err != nil {
+				continue
+			}
+			nDate = nDate / 100
+			if (nToday - nDate) > 1 {
+				continue
+			}
+
+			_, err = objHeader.Write(bLine)
+			if err != nil {
+				log.Println("[WARN] Compress.zipM1Folder() : failed 2 write zip file=", sPath)
+				return nil
+			}
 		}
 
 		return nil
 	})
 
 	if err != nil {
-		log.Println("[ERR] Compress.zipK1Folder() : failed 2 walk src folder :", sSrcFolder)
+		log.Println("[ERR] Compress.zipM1Folder() : failed 2 walk src folder :", sSrcFolder)
 		return false
 	}
 
@@ -128,7 +243,6 @@ func (pSelf *Compress) zipFolder(sDestFile, sSrcFolder string) bool {
 		return false
 	}
 
-	nToday := time.Now().Year()*100 + int(time.Now().Month())
 	objZipFile, err := os.Create(sDestFile)
 	objZipWriter := zip.NewWriter(objZipFile)
 	defer objZipFile.Close()
@@ -174,30 +288,10 @@ func (pSelf *Compress) zipFolder(sDestFile, sSrcFolder string) bool {
 			return nil
 		}
 
-		bytesData, err := ioutil.ReadAll(objFile)
+		_, err = io.Copy(objHeader, objFile)
 		if err != nil {
 			log.Println("[WARN] Compress.zipFolder() : failed 2 read file=", sPath)
 			return nil
-		}
-		for _, bLine := range bytes.Split(bytesData, []byte("\n")) {
-			sFirstFields := strings.Split(string(bLine), ",")[0]
-			if len(sFirstFields) <= 0 {
-				continue
-			}
-			nDate, err := strconv.Atoi(sFirstFields)
-			if err != nil {
-				continue
-			}
-			nDate = nDate / 100
-			if (nToday - nDate) > 1 {
-				continue
-			}
-
-			_, err = objHeader.Write(bLine)
-			if err != nil {
-				log.Println("[WARN] Compress.zipFolder() : failed 2 write zip file=", sPath)
-				return nil
-			}
 		}
 
 		return nil
