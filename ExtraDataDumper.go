67a0b6e3de7920142b201856d957c8896973d794
barry.paneer <barry.paneer@qq.com>
2018-05-31 13:39:25 +0800

    updates

--- ExtraDataDumper.go
+++ ExtraDataDumper.go
@@ -15,7 +15,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 )
 
 var (
@@ -38,11 +37,109 @@ func init() {
 	flag.IntVar(&nTTL, "ttl", 3600*6, " (time to live (default: 3600 * 6 seconds)")
 	flag.StringVar(&sLogFile, "logpath", "./FtpData.log", "log file's path (default:./FtpData.log)")
 	flag.BoolVar(&bDumpLog, "dumplog", false, "a switch 4 log dump (default:false)")
-	flag.StringVar(&sTmpFolder, "dir", "./FtpCache/", "data folder path (default :./FtpCache/)")
+	flag.StringVar(&sTmpFolder, "dir", "./HKSE/", "data folder path (default :./HKSE/)")
 	// [Mandatory]
 	flag.StringVar(&sAccount, "account", "", "login user name (default: '' ")
 	flag.StringVar(&sPassword, "password", "", "login password () default : '' ")
 	flag.Parse()
+	// Generate Folder Name
+	sTmpFolder = strings.Replace(sTmpFolder, "\\", "/", -1)[:strings.LastIndex(sTmpFolder, "/")]
+}
+
+// FTP Folder Downloader
+type FTPFolderSync struct {
+	SpecifyFile  string     // Download Specify File
+	LocalFolder  string     // Download Folder
+	FTPResFolder string     // FTP Resources Folder
+	FTPHandlePtr *goftp.FTP // FTP Handle Pointer
+}
+
+func (pSelf *FTPFolderSync) FilesSync() int {
+	var err error
+	var nFetchCount int = 0
+	// Build Dirs
+	err = os.MkdirAll(pSelf.LocalFolder, 0755)
+	if err != nil {
+		log.Println("[ERR] FTPFolderSync::FilesSync() : cannot build folder : ", pSelf.LocalFolder, err.Error())
+		return nFetchCount
+	}
+	// Download Single File
+	if pSelf.SpecifyFile != "" {
+		sFilePath := strings.Replace(filepath.Join(pSelf.FTPResFolder, pSelf.SpecifyFile), "\\", "/", -1)
+		sLocalFile := strings.Replace(filepath.Join(pSelf.LocalFolder, pSelf.SpecifyFile), "\\", "/", -1)
+
+		_, err = os.Stat(sLocalFile)
+		if err != nil {
+			if os.IsNotExist(err) {
+				log.Printf("[INF] FTPFolderSync::FilesSync() : downloading file : %s --> %s", sFilePath, sLocalFile)
+				_, err = pSelf.FTPHandlePtr.Retr(sFilePath, func(r io.Reader) error {
+					fw, err := os.OpenFile(sLocalFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+					if err != nil {
+						log.Println("[ERR] FTPFolderSync::FilesSync() : cannot create file, file name =", sLocalFile, err.Error())
+						os.Exit(-100)
+					}
+
+					_, err = io.Copy(fw, r)
+					if err != nil {
+						log.Println("[ERR] FTPFolderSync::FilesSync() : cannot write 2 file, file name =", sLocalFile, err.Error())
+						fw.Close()
+						os.Exit(-100)
+					}
+
+					fw.Close()
+					nFetchCount += 1
+					log.Printf("[Done] FTPFolderSync::FilesSync() : Ftp File : FTP[%s] ---> LOCAL[%s], Fetch %d Files", sFilePath, sLocalFile, nFetchCount)
+
+					return err
+				})
+			}
+		}
+
+		log.Printf("[INF] FTPFolderSync::FilesSync() : ignore file : %s --> %s", sFilePath, sLocalFile)
+
+		return nFetchCount
+	}
+	// Walk Ftp && Download
+	err = pSelf.FTPHandlePtr.Walk(pSelf.FTPResFolder, func(path string, info os.FileMode, err error) error {
+		path = strings.Replace(path, "\\", "/", -1)
+		sFileName := path[strings.LastIndex(path, "/"):]
+		sLocalFile := filepath.Join(pSelf.LocalFolder, sFileName)
+		sLocalFile = strings.Replace(sLocalFile, "\\", "/", -1)
+
+		//log.Println(path, sLocalFile)
+		_, err = os.Stat(sLocalFile)
+		if err != nil {
+			if os.IsNotExist(err) {
+				_, err = pSelf.FTPHandlePtr.Retr(path, func(r io.Reader) error {
+					fw, err := os.OpenFile(sLocalFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+					if err != nil {
+						log.Println("[ERR] FTPFolderSync::FilesSync() : cannot create file, file name =", sLocalFile, err.Error())
+						os.Exit(-100)
+					}
+
+					_, err = io.Copy(fw, r)
+					if err != nil {
+						log.Println("[ERR] FTPFolderSync::FilesSync() : cannot write 2 file, file name =", sLocalFile, err.Error())
+						fw.Close()
+						os.Exit(-100)
+					}
+
+					fw.Close()
+					nFetchCount += 1
+
+					return err
+				})
+
+				return nil
+			}
+		}
+		//log.Printf("[INF] FTPFolderSync::FilesSync() : ignore file : %s --> %s", path, sLocalFile)
+		return nil
+	})
+
+	log.Printf("[Done] FTPFolderSync::FilesSync() : Ftp File : FTP[%s] ---> LOCAL[%s], Fetch %d Files", pSelf.FTPResFolder, pSelf.LocalFolder, nFetchCount)
+
+	return nFetchCount
 }
 
 // Program Entry Function
@@ -56,11 +153,6 @@ func main() {
 
 		log.SetOutput(oLogFile)
 	}
-
-	sTmpFolder = strings.Replace(sTmpFolder, "\\", "/", -1)
-	sTmpFolder := sTmpFolder[:strings.LastIndex(sTmpFolder, "/")]
-	sTmpFolder = fmt.Sprintf("%s.%d", sTmpFolder, time.Now().Unix())
-	sTmpFolder += "/"
 	log.Println("[INF] [Ver] ######### 1.0.1 ####################")
 	log.Println("[INF] [Begin] ##################################")
 	/////////////// Ftp ///////////////////////////////////////////////////////////
@@ -91,168 +183,28 @@ func main() {
 	}
 
 	/////////////// Download ./Participant.txt ////////////////////////////////////
-	_, err = ftp.Retr("./Participant.txt", func(r io.Reader) error {
-		sLocalFile := filepath.Join(sTmpFolder, "Participant.txt")
-		fw, err := os.OpenFile(sLocalFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-		if err != nil {
-			log.Println("[ERR] cannot create file, file name =", sLocalFile, err.Error())
-			os.Exit(-100)
-		}
-
-		_, err = io.Copy(fw, r)
-		if err != nil {
-			log.Println("[ERR] cannot write 2 file, file name =", sLocalFile, err.Error())
-			fw.Close()
-			os.Exit(-100)
-		}
-
-		fw.Close()
-		return err
-	})
-	log.Println("[INF] [Done] Ftp File : ./Participant.txt")
+	objParticipantSync := FTPFolderSync{LocalFolder: sTmpFolder, FTPResFolder: "/", SpecifyFile: "Participant.txt", FTPHandlePtr: ftp}
+	objParticipantSync.FilesSync()
 	/////////////// Download shase_rzrq_by_date ////////////////////////////////////
-	sLocalFolder := filepath.Join(sTmpFolder, "shase_rzrq_by_date/")
-	err = os.MkdirAll(sLocalFolder, 0755)
-	if err != nil {
-		log.Println("[ERR] cannot build shase_rzrq_by_date/ folder : ", sLocalFolder, err.Error())
-		return
-	}
-	err = ftp.Walk("/shase_rzrq_by_date/", func(path string, info os.FileMode, err error) error {
-		_, err = ftp.Retr(path, func(r io.Reader) error {
-			path = strings.Replace(path, "\\", "/", -1)
-			sFileName := path[strings.LastIndex(path, "/"):]
-			sLocalFile := filepath.Join(sLocalFolder, sFileName)
-
-			fw, err := os.OpenFile(sLocalFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-			if err != nil {
-				log.Println("[ERR] cannot create file, file name =", sLocalFile, err.Error())
-				os.Exit(-100)
-			}
-
-			_, err = io.Copy(fw, r)
-			if err != nil {
-				log.Println("[ERR] cannot write 2 file, file name =", sLocalFile, err.Error())
-				fw.Close()
-				os.Exit(-100)
-			}
-
-			fw.Close()
-
-			return err
-		})
-
-		return nil
-	})
-	log.Println("[INF] [Done] Ftp File : ./shase_rzrq_by_date/")
+	objSHRzrqSync := FTPFolderSync{LocalFolder: filepath.Join(sTmpFolder, "shase_rzrq_by_date/"), FTPResFolder: "/shase_rzrq_by_date/", FTPHandlePtr: ftp}
+	objSHRzrqSync.FilesSync()
 	/////////////// Download sznse_rzrq_by_date ////////////////////////////////////
-	sLocalFolder = filepath.Join(sTmpFolder, "sznse_rzrq_by_date/")
-	err = os.MkdirAll(sLocalFolder, 0755)
-	if err != nil {
-		log.Println("[ERR] cannot build sznse_rzrq_by_date/ folder : ", sLocalFolder, err.Error())
-		return
-	}
-	err = ftp.Walk("/sznse_rzrq_by_date/", func(path string, info os.FileMode, err error) error {
-		_, err = ftp.Retr(path, func(r io.Reader) error {
-			path = strings.Replace(path, "\\", "/", -1)
-			sFileName := path[strings.LastIndex(path, "/"):]
-			sLocalFile := filepath.Join(sLocalFolder, sFileName)
-
-			fw, err := os.OpenFile(sLocalFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-			if err != nil {
-				log.Println("[ERR] cannot create file, file name =", sLocalFile, err.Error())
-				os.Exit(-100)
-			}
-
-			_, err = io.Copy(fw, r)
-			if err != nil {
-				log.Println("[ERR] cannot write 2 file, file name =", sLocalFile, err.Error())
-				fw.Close()
-				os.Exit(-100)
-			}
-
-			fw.Close()
-
-			return err
-		})
-
-		return nil
-	})
-	log.Println("[INF] [Done] Ftp File : ./sznse_rzrq_by_date/")
+	objSZRzrqSync := FTPFolderSync{LocalFolder: filepath.Join(sTmpFolder, "sznse_rzrq_by_date/"), FTPResFolder: "/sznse_rzrq_by_date/", FTPHandlePtr: ftp}
+	objSZRzrqSync.FilesSync()
 	/////////////// Download shsz_idx_by_date/ ////////////////////////////////////
-	sLocalFolder = filepath.Join(sTmpFolder, "shsz_idx_by_date/")
-	err = os.MkdirAll(sLocalFolder, 0755)
-	if err != nil {
-		log.Println("[ERR] cannot build shsz_idx_by_date/ folder : ", sLocalFolder, err.Error())
-		return
-	}
-	err = ftp.Walk("/shsz_idx_by_date/", func(path string, info os.FileMode, err error) error {
-		_, err = ftp.Retr(path, func(r io.Reader) error {
-			path = strings.Replace(path, "\\", "/", -1)
-			sFileName := path[strings.LastIndex(path, "/"):]
-			sLocalFile := filepath.Join(sLocalFolder, sFileName)
-
-			fw, err := os.OpenFile(sLocalFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-			if err != nil {
-				log.Println("[ERR] cannot create file, file name =", sLocalFile, err.Error())
-				os.Exit(-100)
-			}
-
-			_, err = io.Copy(fw, r)
-			if err != nil {
-				log.Println("[ERR] cannot write 2 file, file name =", sLocalFile, err.Error())
-				fw.Close()
-				os.Exit(-100)
-			}
-
-			fw.Close()
-
-			return err
-		})
-
-		return nil
-	})
-	log.Println("[INF] [Done] Ftp File : ./shsz_idx_by_date/")
+	objSHSZIndexSync := FTPFolderSync{LocalFolder: filepath.Join(sTmpFolder, "shsz_idx_by_date/"), FTPResFolder: "/shsz_idx_by_date/", FTPHandlePtr: ftp}
+	objSHSZIndexSync.FilesSync()
 	/////////////// Download shsz_detail/ ////////////////////////////////////
-	sLocalFolder = filepath.Join(sTmpFolder, "shsz_detail/")
-	err = os.MkdirAll(sLocalFolder, 0755)
-	if err != nil {
-		log.Println("[ERR] cannot build shsz_detail/ folder : ", sLocalFolder, err.Error())
-		return
-	}
-	err = ftp.Walk("/shsz_detail/", func(path string, info os.FileMode, err error) error {
-		_, err = ftp.Retr(path, func(r io.Reader) error {
-			path = strings.Replace(path, "\\", "/", -1)
-			sFileName := path[strings.LastIndex(path, "/"):]
-			sLocalFile := filepath.Join(sLocalFolder, sFileName)
+	objSHSZDetailSync := FTPFolderSync{LocalFolder: filepath.Join(sTmpFolder, "shsz_detail/"), FTPResFolder: "/shsz_detail/", FTPHandlePtr: ftp}
+	objSHSZDetailSync.FilesSync()
 
-			fw, err := os.OpenFile(sLocalFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-			if err != nil {
-				log.Println("[ERR] cannot create file, file name =", sLocalFile, err.Error())
-				os.Exit(-100)
-			}
-
-			_, err = io.Copy(fw, r)
-			if err != nil {
-				log.Println("[ERR] cannot write 2 file, file name =", sLocalFile, err.Error())
-				fw.Close()
-				os.Exit(-100)
-			}
-
-			fw.Close()
-
-			return err
-		})
-
-		return nil
-	})
-	log.Println("[INF] [Done] Ftp File : ./shsz_detail/")
-
-	os.RemoveAll("./HKSE")
-	err = os.Rename(sTmpFolder, "./HKSE/")
-	if err != nil {
-		log.Println("[ERR] cannot remove folder, folder name =", sTmpFolder, err.Error())
-		return
-	}
+	////////////// Rename TmpFolder Name
+	/*	os.RemoveAll("./HKSE")
+		err = os.Rename(sTmpFolder, "./HKSE/")
+		if err != nil {
+			log.Println("[ERR] cannot remove folder, folder name =", sTmpFolder, err.Error())
+			return
+		}*/
 
 	log.Println("[INF] [ End ] ##################################")
 }
