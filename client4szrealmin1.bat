1cdb8439fc38475c00ab5ca07ec5a7ed7e953168
barry.paneer <barry.paneer@qq.com>
2018-06-12 11:06:37 +0800

    updates

--- client4szrealmin1.bat
+++ client4szrealmin1.bat
@@ -1,1 +1,1 @@
-client.exe --uri=SZSE/MIN1_TODAY --ip=61.174.50.183 --account=guest --password=guest
+client.exe --dumplog=true --uri=SZSE/MIN1_TODAY --ip=61.174.50.183 --account=guest --password=guest
